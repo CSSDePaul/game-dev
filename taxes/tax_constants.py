c8675b403897e0910c3df9e5737a53841c828942
humblevladimirthegreat <humblevladimirthegreat@gmx.com>
2015-02-10 17:13:21 -1200

    implemented randomHole and tax_constants

---
+++ taxes/tax_constants.py
@@ -0,0 +1,21 @@
+GAME_TITLE = 'Taxes'
+FPS = 30 # frames per second, the general speed of the program
+WINDOWWIDTH = 640 # size of window's width in pixels
+WINDOWHEIGHT = 480 # size of windows' height in pixels
+
+#            R    G    B
+GRAY     = (100, 100, 100)
+NAVYBLUE = ( 60,  60, 100)
+WHITE    = (255, 255, 255)
+RED      = (255,   0,   0)
+GREEN    = (  0, 255,   0)
+BLUE     = (  0,   0, 255)
+YELLOW   = (255, 255,   0)
+ORANGE   = (255, 128,   0)
+PURPLE   = (255,   0, 255)
+CYAN     = (  0, 255, 255)
+BLACK    = (  0,   0,   0)
+
+BGCOLOR = NAVYBLUE
+LIGHTBGCOLOR = GRAY
+ALLCOLORS = (RED, GREEN, BLUE, YELLOW, ORANGE, PURPLE, CYAN)
--- taxes/taxhole.py
+++ taxes/taxhole.py
@@ -1,16 +1,22 @@
 import math
 import pygame
+from tax_constants import *
+import random
 
 def initializeHoles():
 	holes = set()
-	holes.add(Hole(200,200, 10, 0, 10))
+	for i in range(5):
+		holes.add(randomHole())
 	return holes
 
 def randomHole():
-	pass
+	x = random.randint(0, WINDOWWIDTH)
+	y = random.randint(0, WINDOWHEIGHT)
+	r = random.randint(5,15)
+	a = random.uniform(0, 2*math.pi)
+	s = random.randint(5,15)
+	return Hole(x, y, r, a, s)
 	
-BLACK = (0,0,0)
-
 class Hole:
 	
 	def __init__(self, xpos, ypos, radius, angle, speed):
@@ -23,13 +29,21 @@ class Hole:
 	def update(self):
 		self.xpos += int(math.cos(self.angle)*self.speed)
 		self.ypos += int(math.sin(self.angle)*self.speed)
-		self.checkBounce()
+		self.checkOffScreen()
 		
 	def checkOffScreen(self):
-		pass
+		if self.xpos < 0:
+			self.xpos = WINDOWWIDTH
+		if self.ypos < 0:
+			self.ypos = WINDOWHEIGHT
+		if self.xpos > WINDOWWIDTH:
+			self.xpos = 0
+		if self.ypos > WINDOWHEIGHT:
+			self.ypos = 0
+			
 		
 	def randomizeTrajectory():
-		pass
+		pass 
 		
 	def draw(self, displaysurf):
 		pygame.draw.circle(displaysurf, BLACK, (self.xpos, self.ypos), self.radius, 0)
